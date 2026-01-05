2fda186e279a8d2430adcd889e95ae484f83521d
ddilshod2197 <ddilshod2197@gmail.com>
2026-01-05 12:34:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import geometriya
+
+r = 7
+res1 = geometriya.doira_yuzi(7)
+print(res1)
+
+a = 5
+res2 = geometriya.kv_yuzi(a)
+print(res2)
+
+a = 6
+h = 4
+res3 = geometriya.ucbrck_yuzi(a, h)
+print(res3)
